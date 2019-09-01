aab28515d375f7c97c2579cf7aa7841c013abc4f
Andrew Obiano <andrew.obiano@gmail.com>
2019-08-31 15:39:36 -1000

    Finished

--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -1,5 +1,7 @@
 -- db indexed_cars
 
+DROP TABLE IF EXISTS car_models;
+
 CREATE TABLE IF NOT EXISTS car_models
 (
  id serial,
@@ -9,4 +11,13 @@ CREATE TABLE IF NOT EXISTS car_models
  model_title character varying(125) NOT NULL,
  year integer NOT NULL,
  PRIMARY KEY (id)
-)
+);
+
+CREATE INDEX make_model_title
+ON car_models (make_title, model_title);
+
+CREATE INDEX make_model_code
+ON car_models (make_code, model_code);
+
+CREATE INDEX car_year
+ON car_models (year);
